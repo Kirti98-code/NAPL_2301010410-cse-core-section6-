174fc9e9c12d4e61abe5fa4e7e14882e4493a8c1
Kirti98-code <kirtigautam521@gmail.com>
2026-08-11 12:28:28 +0530

    Add files via upload

---
+++ NAPL/lab1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	var name string = "kirti"
+	var rollnumber int = 2301010410
+	fmt.Printf("Name: %s, Roll Number: %d\n", name, rollnumber)
+	//
+	// code 3
+	num1 := 10
+	num2 := 20
+	sum := num1 + num2
+	fmt.Printf("Sum of %d and %d is %d\n", num1, num2, sum)
+}
