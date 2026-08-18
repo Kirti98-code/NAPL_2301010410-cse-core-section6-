42bf9f3578cc99bca16319fa9d3613361efe16c3
Kirti98-code <kirtigautam521@gmail.com>
2026-08-18 12:08:32 +0530

    Add files via upload

---
+++ lab1_Q1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, kirti!")
+	fmt.Println("Integer operations")
+	num1 := 10
+	num2 := 20
+	fmt.Printf("Sum : %d\n", num1+num2)
+	fmt.Printf("Difference : %d\n", num1-num2)
+	fmt.Printf("Multiplication : %d\n", num1*num2)
+	fmt.Printf("Division : %d\n", num1/num2)
+
+	fmt.Println("Float operations")
+	num3 := 10.5
+	num4 := 20.9
+	fmt.Printf("Sum : %f\n", num3+num4)
+	fmt.Printf("Difference : %f\n", num3-num4)
+	fmt.Printf("Multiplication : %f\n", num3*num4)
+	fmt.Printf("Division : %f\n", num3/num4)
+
+}
